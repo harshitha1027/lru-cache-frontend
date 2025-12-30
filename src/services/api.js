ac584a97c8f7b48b7c9de9a03eaece137f819956
harshitha1027 <153825726+harshitha1027@users.noreply.github.com>
2025-12-30 19:16:26 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,32 @@
+// export const simulateCache = async (cacheSize, pages) => {
+//   try {
+//     const response = await fetch("http://localhost:8080/api/cache/simulate", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ cacheSize, pages }),
+//     });
+//     return await response.json();
+//   } catch (err) {
+//     console.error("Error calling backend:", err);
+//     return null;
+//   }
+// };
+
 export const simulateCache = async (cacheSize, pages) => {
   try {
-    const response = await fetch("http://localhost:8080/api/cache/simulate", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ cacheSize, pages }),
-    });
+    const response = await fetch(
+      "https://lru-cache-backend-hddj.onrender.com/api/cache/simulate",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ cacheSize, pages }),
+      }
+    );
+
     return await response.json();
   } catch (err) {
     console.error("Error calling backend:", err);
     return null;
   }
 };
+
